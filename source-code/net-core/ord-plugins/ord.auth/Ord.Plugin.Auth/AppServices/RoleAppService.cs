398f8ff21c66a1bed835d5c58df4e910fffa91b3
DESKTOP-SH01Q5E\Admin <huongcv@orenda.vn>
2025-06-10 16:14:02 +0700

    RoleAppService

--- source-code/net-core/ord-plugins/ord.auth/Ord.Plugin.Auth/AppServices/RoleAppService.cs
+++ source-code/net-core/ord-plugins/ord.auth/Ord.Plugin.Auth/AppServices/RoleAppService.cs
@@ -1,12 +1,53 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.AspNetCore.Mvc;
+using Ord.Plugin.Auth.Base;
+using Ord.Plugin.Auth.Shared.Dtos;
+using Ord.Plugin.Auth.Shared.Entities;
+using Ord.Plugin.Auth.Shared.Repositories;
+using Ord.Plugin.Contract.Dtos;
+using Volo.Abp.Application.Dtos;
 
 namespace Ord.Plugin.Auth.AppServices
 {
-    internal class RoleAppService
+    [OrdAuth]
+    public class RoleAppService : OrdAuthAppService
     {
+        private IRoleCrudRepository RoleCrudRepository => AppFactory.GetServiceDependency<IRoleCrudRepository>();
+        [HttpPost]
+        [OrdAuth("AuthPlugin.Role")]
+        public async Task<CommonResultDto<PagedResultDto<RolePagedDto>>> GetPaged(RolePagedInput input)
+        {
+            var paged = await RoleCrudRepository.GetPagedListAsync(input);
+            return AppFactory.CreateSuccessResult(paged);
+        }
+        [HttpPost]
+        [OrdAuth("AuthPlugin.Role")]
+        public async Task<CommonResultDto<CounterByIsActivedDto>> GetCountByIsActived(RolePagedInput input)
+        {
+            return AppFactory.CreateSuccessResult(await RoleCrudRepository.GetCountGroupByIsActivedAsync(input));
+        }
+        [HttpPost]
+        [OrdAuth("AuthPlugin.Role")]
+        public async Task<CommonResultDto<RoleDetailDto>> GetById(EncodedIdDto input)
+        {
+            return AppFactory.CreateSuccessResult(await RoleCrudRepository.GetDetailByEncodedIdAsync(input.EncodedId));
+        }
+        [HttpPost]
+        [OrdAuth("AuthPlugin.Role.Create")]
+        public async Task<CommonResultDto<RoleDetailDto>> CreateAsync(CreateRoleDto input)
+        {
+            var createUser = await RoleCrudRepository.CreateAsync(input);
+            return AppFactory.CreateSuccessResult(AppFactory.ObjectMap<RoleEntity, RoleDetailDto>(createUser));
+        }
+        [HttpPost]
+        [OrdAuth("AuthPlugin.Role.Update")]
+        public async Task<CommonResultDto<RoleDetailDto>> UpdateAsync(UpdateRoleDto input)
+        {
+            var updatedRole = await RoleCrudRepository.UpdateByEncodedIdAsync(input.EncodedId, input);
+            if (updatedRole == null)
+            {
+                return CreateNotFoundResult<RoleDetailDto>("crud_user_not_found");
+            }
+            return AppFactory.CreateSuccessResult(AppFactory.ObjectMap<RoleEntity, RoleDetailDto>(updatedRole));
+        }
     }
 }
