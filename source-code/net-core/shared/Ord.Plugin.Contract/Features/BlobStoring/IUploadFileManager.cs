6855eb2aeb8f2d8d9b7c87573dad9d15db2d1863
DESKTOP-SH01Q5E\Admin <huongcv@orenda.vn>
2025-06-18 14:58:14 +0700

    UploadFileManager

--- source-code/net-core/shared/Ord.Plugin.Contract/Features/BlobStoring/IUploadFileManager.cs
+++ source-code/net-core/shared/Ord.Plugin.Contract/Features/BlobStoring/IUploadFileManager.cs
@@ -6,8 +6,8 @@ namespace Ord.Plugin.Contract.Features.BlobStoring
     public interface IUploadFileManager : IDomainService
     {
         Task<FileUploadDto> GetByFileIdAsync(Guid fileId);
-        Task<FileUploadDto> UploadFileAsync(IFormFile file, string blobContainerPath);
-        Task<FileUploadDto> UploadFileAsync(byte[] fileByte, string fileName, string mimeType, string blobContainerPath);
+        Task<FileUploadDto> UploadFileAsync(FileStoreProvider storeProvider, IFormFile file, string blobContainerPath);
+        Task<FileUploadDto> UploadFileAsync(FileStoreProvider storeProvider, byte[] fileByte, string fileName, string mimeType, string blobContainerPath);
         Task RemoveFileAsync(Guid fileId);
     }
 }
---
+++ source-code/net-core/shared/Ord.Plugin.Core/Features/BlobStoring/UploadFileManager.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Ord.Plugin.Contract.Features.BlobStoring;
+using Ord.Plugin.Core.Base;
+
+namespace Ord.Plugin.Core.Features.BlobStoring
+{
+    public class UploadFileManager : OrdManagerBase, IUploadFileManager
+    {
+        public Task<FileUploadDto> GetByFileIdAsync(Guid fileId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<FileUploadDto> UploadFileAsync(FileStoreProvider storeProvider, IFormFile file, string blobContainerPath)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<FileUploadDto> UploadFileAsync(FileStoreProvider storeProvider, byte[] fileByte, string fileName, string mimeType,
+            string blobContainerPath)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task RemoveFileAsync(Guid fileId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
