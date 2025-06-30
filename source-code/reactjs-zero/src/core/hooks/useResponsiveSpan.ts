78b5da9bc15c5e582c4a57756a48803d1ceb9ce3
DESKTOP-SH01Q5E\Admin <huongcv@orenda.vn>
2025-06-30 12:13:45 +0700

    done footer modal

--- source-code/reactjs-zero/src/core/hooks/useResponsiveSpan.ts
+++ source-code/reactjs-zero/src/core/hooks/useResponsiveSpan.ts
@@ -16,7 +16,7 @@ export const useResponsiveSpan = (
         const getResponsiveSpan = (): ColProps => {
             // Dựa trên width mong muốn cho XL (tham số truyền vào), và scale dần cho các breakpoint khác
             const xl = width;
-            const lg = width >= 24 ? 24 : width >= 16 ? 16 : width >= 8 ? 8 : 6;
+            const lg = width <= 8 ? 8 : width <= 12 ? 12 : width <= 16 ? 16 : 24;
             const md = width >= 16 ? 24 : 12;
             const sm = 24;
             const xs = 24;
